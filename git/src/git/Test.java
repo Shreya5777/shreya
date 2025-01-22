db1c7ccdf96e227ea9963413418b840823124e96
shrey <shrey@Shreya>
2025-01-22 18:06:54 +0530

    add by shreya

---
+++ git/src/git/Test.java
@@ -0,0 +1,17 @@
+package git;
+
+public class Test {
+	public void addition()
+	{
+		System.out.println ("addition");
+	}
+		
+			
+		
+	public static void main(String[] args) {
+		
+		}
+		
+	}
+
+
